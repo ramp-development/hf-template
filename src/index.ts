e255386edf893100b5be43119eb84e3e01428449
g4knr <85592362+g4knr@users.noreply.github.com>
2023-02-21 01:43:48 +0000

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { greetUser } from '$utils/greet';
+import { gear } from './pages/gear';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  // define the follow attribute for links
+  const links = [...document.querySelectorAll('a')];
+  links.forEach((link) => {
+    const follow = link.host === window.location.host ? 'follow' : 'nofollow';
+    link.setAttribute('rel', follow);
+  });
+
+  // run page specific code
+  const { pathname } = window.location;
+  if (pathname.includes('/gear/')) gear();
 });
---
+++ src/pages/gear.ts
@@ -0,0 +1,36 @@
+export const gear = () => {
+  recommendedReads();
+
+  function recommendedReads() {
+    const wrapper = document.querySelector('[data-recommended="wrapper"]');
+    if (!wrapper) return;
+
+    // get the two groups of links
+    const main = wrapper.querySelector('[data-recommended="main"]'),
+      sub = wrapper.querySelector('[data-recommended="sub"]');
+
+    // get the main items and remove the sub group if there are enough
+    const mainLinks = [...main.querySelectorAll('[data-recommended="link"]')];
+    if (mainLinks.length === 3) {
+      sub.remove();
+      return;
+    }
+
+    // find out what we already have and how many we need
+    const includedLinks = mainLinks.map((link) => link.href);
+    let numberRequired = 3 - mainLinks.length;
+
+    // go through the sub links and add/remove as required
+    const subLinks = [...sub.querySelectorAll('[data-recommended="link"]')];
+    subLinks.forEach((link) => {
+      // remove the item if there are already enough or if it is already included
+      if (numberRequired === 0 || includedLinks.includes(link.href)) {
+        link.remove();
+        return;
+      }
+
+      // keep the item and decrement the remaining required
+      numberRequired -= 1;
+    });
+  }
+};
